4960661ab496645bff192387aa7bb2cd1dcd7b81
prachi1211 <prachijethava1211@gmail.com>
2025-03-19 13:12:54 -0400

    change to type.js file

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -14,10 +14,8 @@ function Type() {
       <Typewriter
         options={{
           strings: [
-            "Data Strategist",
             "Software Engineer",
             "Data Analyst",
-            "Data Scientist",
             "Software Developer",
           ],
           autoStart: true,
