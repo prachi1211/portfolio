fdd3115c14bf999ac4f5487e7c6b069b4292d103
prachi1211 <prachijethava1211@gmail.com>
2025-06-07 12:52:24 -0400

    Updated Resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import { AiOutlineDownload } from "react-icons/ai";
 // import pdf from "../../Assets/Prachi_Piyushbhai_JethavaSE.pdf";
-import pdf from "../../Assets/Prachi Piyushbhai Jethava.pdf"
+import pdf from "../../Assets/Prachi_Piyushbhai_Jethava_resume.pdf"
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
